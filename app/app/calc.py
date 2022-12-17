b3bdd8fdd2973e62378482b69e9cda9c9767122c
daniel <danielsmag11@gmail.com>
2022-12-17 11:59:03 +0200

    first commit

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+from typing import Union
+
+"""
+calculater function
+"""
+
+
+def add(a: int, b: int) -> Union[int, float]:
+    return a + b
+
+
+def subtract(a: int, b: int) -> Union[int, float]:
+    return a - b
